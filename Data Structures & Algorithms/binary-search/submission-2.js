d8096590560ea2baf556f81bacbbb7d50f4b1021
ZaneVR <103779279+GodRAIZEL@users.noreply.github.com>
2026-08-17 11:04:32 +0200

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let lower = 0;
+        let upper = nums.length-1;
+        while(lower<=upper){
+            let middle = Math.ceil((upper + lower)/2);
+            if(nums[middle] === target){
+                return middle;
+            }
+            else if(nums[middle] < target){
+                lower = middle+1;
+            }
+            else if(nums[middle] > target){
+                upper = middle-1;
+            }
+
+        }
+
+        return -1;
+    }
+}
