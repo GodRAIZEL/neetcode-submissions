0e24f13b39403e24c5dd657a8cdf6b0d06a7c08b
ZaneVR <103779279+GodRAIZEL@users.noreply.github.com>
2026-09-16 15:01:25 +0200

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s, t) {
+        let targetHashMap = {};
+        let sHashMap = {};
+        let matches = 0;
+        let left = 0;
+        let right = 0;
+        let outputString = "";
+
+        for (let char of t) {
+            targetHashMap[char] = (targetHashMap[char] || 0) + 1;
+        }
+
+        const targetMatch = Object.keys(targetHashMap).length;
+
+        while (right < s.length) {
+            let rightChar = s[right];
+
+            sHashMap[rightChar] = (sHashMap[rightChar] || 0) + 1;
+
+            if (targetHashMap[rightChar] === sHashMap[rightChar]) {
+                matches++;
+            }
+
+
+            while (matches === targetMatch) {
+                let leftChar = s[left];
+                if (outputString === "" || outputString.length > right - left + 1) {
+                    outputString = s.slice(left, right + 1);
+                }
+
+                if (targetHashMap[leftChar] === sHashMap[leftChar]) {
+                    matches--;
+                }
+
+                sHashMap[leftChar]--;
+                left++;
+            }
+
+            right++;
+        }
+
+        return outputString;
+
+    }
+}
